e9d1d7d1941350715724498a28d42940282cda49
macetheace22 <245303492+macetheace22@users.noreply.github.com>
2025-11-22 00:35:37 -0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-const supabase = createClient("YOUR_URL","YOUR_ANON_KEY");
+const supabase = createClient("https://lzitbxzwhuiaeynmrybq.supabase.co","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6aXRieHp3aHVpYWV5bm1yeWJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2Njg2NjcsImV4cCI6MjA3OTI0NDY2N30.5PNoFntss-iTlU0Jt6DNDV74VG8G406UVF61OLSuQb0");
 
 document.getElementById("login-btn").addEventListener("click", async () => {
   const { error } = await supabase.auth.signInWithOAuth({
