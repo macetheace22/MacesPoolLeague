160f5e80b8b1e92efc346433a15d799b78d35d29
macetheace22 <245303492+macetheace22@users.noreply.github.com>
2025-12-05 17:01:45 -0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -5,8 +5,8 @@ async function initAdmin() {
   const logoutBtn = document.getElementById('logoutBtn')
   const userEmail = document.getElementById('userEmail')
 
-  loginBtn?.addEventListener('click', () => supabase.auth.signInWithOAuth({ provider: 'google' , options: { redirectTo: location.origin + "MacesPoolLeague/profile.html" }}))
-  //loginBtn?.addEventListener("click", async () => { const { error } = await supabase.auth.signInWithOAuth({provider: "google", options: { redirectTo: location.origin + "/MacesPoolLeague/profile.html" }});if (error) console.error("Login error:", error);});
+  //loginBtn?.addEventListener('click', () => supabase.auth.signInWithOAuth({ provider: 'google' , options: { redirectTo: location.origin + "MacesPoolLeague/profile.html" }}))
+  loginBtn?.addEventListener("click", async () => { const { error } = await supabase.auth.signInWithOAuth({provider: "google", options: { redirectTo: location.origin + "/MacesPoolLeague/profile.html" }});if (error) console.error("Login error:", error);});
   logoutBtn?.addEventListener('click', async () => { await supabase.auth.signOut(); location.reload() })
 
   const { data: { user } } = await supabase.auth.getUser()
